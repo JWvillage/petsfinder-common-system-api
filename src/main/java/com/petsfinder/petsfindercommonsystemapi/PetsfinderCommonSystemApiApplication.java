e4ee8b25ac8899183233815f234d28565d480246
jwhouse <gnsl0127@naver.com>
2023-03-29 13:54:14 +0900

    Initial Commit!

---
+++ src/main/java/com/petsfinder/petsfindercommonsystemapi/PetsfinderCommonSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.petsfinder.petsfindercommonsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsfinderCommonSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsfinderCommonSystemApiApplication.class, args);
+    }
+
+}
